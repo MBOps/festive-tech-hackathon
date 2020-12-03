2fb779d009d40a2fccd20dd162ab3b73a49c940b
Matt Boyd <mjb141@gmail.com>
2020-12-03 20:22:51 +0000

    Test2

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "azurerm_app_service" "webapp" {
 
 resource "azurerm_storage_account" "storage" {
     count                    = length(var.webapplocations)
-    name                     = replace(lower("${var.resource_prefix}-${var.webapplocations[count.index]}-storage"), "-", "")
+    name                     = replace(lower("${var.resource_prefix}-${var.webapplocations[count.index]}-sa"), "-", "")
     location                 = var.webapplocations[count.index]
     resource_group_name      = azurerm_resource_group.rg.name
     account_tier             = "Standard"
