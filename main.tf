2c754166c474148d483c6885b2754b5029de8d39
Matt Boyd <mjb141@gmail.com>
2020-12-04 21:12:46 +0000

    test5

--- main.tf
+++ main.tf
@@ -55,7 +55,8 @@ resource "azurerm_app_service" "webapp" {
         "storageContainerName"          = "${var.resource_prefix}-${var.short_names[each.key]}"
         "connectionString "             = "${azurerm_storage_account.storage[each.key].primary_connection_string}"
     }
-    depends_on = [azurerm_storage_account.storage]
+    depends_on = [azurerm_storage_account.storage, azurerm_app_service.webapp]
+    
 }
 
 resource "azurerm_storage_account" "storage" {
