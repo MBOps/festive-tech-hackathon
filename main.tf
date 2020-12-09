b884373ec709bd23310aabf7ab843599d761fc68
Matt Boyd <mjb141@gmail.com>
2020-12-09 15:15:41 +0000

    Test

--- main.tf
+++ main.tf
@@ -26,8 +26,7 @@ resource "azurerm_app_service_plan" "asp" {
     name                = "${var.resource_prefix}-${var.short_names[each.key]}-asp"
     location            = each.value
     resource_group_name = azurerm_resource_group.rg.name
-    kind                = "Linux"
-    reserved            = true
+    kind                = "Windows"
     sku {
         tier = "Free"
         size = "F1"
