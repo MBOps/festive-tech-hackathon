20332354b1c3b4a5a6f0d6fdb7b80ea06547113b
Matt Boyd <mjb141@gmail.com>
2020-12-09 10:46:17 +0000

    just deploying storageaccounts

--- main.tf
+++ main.tf
@@ -21,43 +21,43 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Provision the App Service plan to host the App Service web app in each region
-resource "azurerm_app_service_plan" "asp" {
-    for_each = var.regions
-    name                = "${var.resource_prefix}-${var.short_names[each.key]}-asp"
-    location            = each.value
-    resource_group_name = azurerm_resource_group.rg.name
-    kind                = "Windows"
+# resource "azurerm_app_service_plan" "asp" {
+#     for_each = var.regions
+#     name                = "${var.resource_prefix}-${var.short_names[each.key]}-asp"
+#     location            = each.value
+#     resource_group_name = azurerm_resource_group.rg.name
+#     kind                = "Windows"
 
-    sku {
-        tier = "Free"
-        size = "F1"
-    }
-}
+#     sku {
+#         tier = "Free"
+#         size = "F1"
+#     }
+# }
 
 # Provision the Azure App Service to host the main web site
-resource "azurerm_app_service" "webapp" {
-    for_each = var.regions
-    name                = "${var.resource_prefix}-${var.short_names[each.key]}-webapp"
-    location            = each.value
-    resource_group_name = azurerm_resource_group.rg.name
-    app_service_plan_id = azurerm_app_service_plan.asp[each.key].id
+# resource "azurerm_app_service" "webapp" {
+#     for_each = var.regions
+#     name                = "${var.resource_prefix}-${var.short_names[each.key]}-webapp"
+#     location            = each.value
+#     resource_group_name = azurerm_resource_group.rg.name
+#     app_service_plan_id = azurerm_app_service_plan.asp[each.key].id
 
-    # site_config {
-        # always_on           = true
-        # default_documents   = [
-        #     "Default.htm",
-        #     "Default.html",
-        #     "hostingstart.html"
-        # ]
-    # }
+#     # site_config {
+#         # always_on           = true
+#         # default_documents   = [
+#         #     "Default.htm",
+#         #     "Default.html",
+#         #     "hostingstart.html"
+#         # ]
+#     # }
 
-    app_settings = {
-        "storageContainerName"          = "${var.resource_prefix}-${var.short_names[each.key]}"
-        "connectionString "             = "${azurerm_storage_account.storage[each.key].primary_connection_string}"
-    }
-    depends_on = [azurerm_storage_account.storage, azurerm_app_service_plan.asp]
+#     app_settings = {
+#         "storageContainerName"          = "${var.resource_prefix}-${var.short_names[each.key]}"
+#         "connectionString "             = "${azurerm_storage_account.storage[each.key].primary_connection_string}"
+#     }
+#     depends_on = [azurerm_storage_account.storage, azurerm_app_service_plan.asp]
     
-}
+# }
 
 resource "azurerm_storage_account" "storage" {
     for_each = var.regions
@@ -68,53 +68,53 @@ resource "azurerm_storage_account" "storage" {
     account_replication_type = "GRS"
 }
 
-resource "azurerm_frontdoor" "frontdoor" {
-  name                                         = "${var.resource_prefix}-frontdoor"
-  location                                     = "Global"
-  resource_group_name                          = azurerm_resource_group.rg.name
-  enforce_backend_pools_certificate_name_check = false
+# resource "azurerm_frontdoor" "frontdoor" {
+#   name                                         = "${var.resource_prefix}-frontdoor"
+#   #location                                     = "Global"
+#   resource_group_name                          = azurerm_resource_group.rg.name
+#   enforce_backend_pools_certificate_name_check = false
 
-  routing_rule {
-    name               = "${var.resource_prefix}-RoutingRule1"
-    accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = ["/*"]
-    frontend_endpoints = ["${var.resource_prefix}-FrontendEndpoint1"]
-    forwarding_configuration {
-      forwarding_protocol = "MatchRequest"
-      backend_pool_name   = "${var.resource_prefix}-Backend"
-    }
-  }
+#   routing_rule {
+#     name               = "${var.resource_prefix}-RoutingRule1"
+#     accepted_protocols = ["Http", "Https"]
+#     patterns_to_match  = ["/*"]
+#     frontend_endpoints = ["${var.resource_prefix}-FrontendEndpoint1"]
+#     forwarding_configuration {
+#       forwarding_protocol = "MatchRequest"
+#       backend_pool_name   = "${var.resource_prefix}-Backend"
+#     }
+#   }
 
-  backend_pool_load_balancing {
-    name = "${var.resource_prefix}-LoadBalancingSettings1"
-  }
+#   backend_pool_load_balancing {
+#     name = "${var.resource_prefix}-LoadBalancingSettings1"
+#   }
 
-  backend_pool_health_probe {
-    name = "${var.resource_prefix}-HealthProbeSetting1"
-  }
+#   backend_pool_health_probe {
+#     name = "${var.resource_prefix}-HealthProbeSetting1"
+#   }
 
-  backend_pool {
-    name = "${var.resource_prefix}-Backend"
+#   backend_pool {
+#     name = "${var.resource_prefix}-Backend"
     
-    dynamic backend {
-      for_each = var.regions
+#     dynamic backend {
+#       for_each = var.regions
 
-      content {
-        host_header = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
-        address     = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
-        http_port   = 80
-        https_port  = 443
-      }
-    }
+#       content {
+#         host_header = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
+#         address     = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
+#         http_port   = 80
+#         https_port  = 443
+#       }
+#     }
 
-    load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
-    health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
-  }
+#     load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
+#     health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
+#   }
 
-  frontend_endpoint {
-    name                              = "${var.resource_prefix}-FrontendEndpoint1"
-    host_name                         = "${var.resource_prefix}-frontdoor.azurefd.net"
-    custom_https_provisioning_enabled = false
-  }
-  depends_on = [azurerm_app_service.webapp]
-}
+#   frontend_endpoint {
+#     name                              = "${var.resource_prefix}-FrontendEndpoint1"
+#     host_name                         = "${var.resource_prefix}-frontdoor.azurefd.net"
+#     custom_https_provisioning_enabled = false
+#   }
+#   depends_on = [azurerm_app_service.webapp]
+# }
