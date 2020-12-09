1bafd6b2f444383529f2b4b4ff294909813f12f7
Matt Boyd <mjb141@gmail.com>
2020-12-09 10:58:52 +0000

    Adding WebApps back in

--- main.tf
+++ main.tf
@@ -35,29 +35,29 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 # Provision the Azure App Service to host the main web site
-# resource "azurerm_app_service" "webapp" {
-#     for_each = var.regions
-#     name                = "${var.resource_prefix}-${var.short_names[each.key]}-webapp"
-#     location            = each.value
-#     resource_group_name = azurerm_resource_group.rg.name
-#     app_service_plan_id = azurerm_app_service_plan.asp[each.key].id
+resource "azurerm_app_service" "webapp" {
+    for_each = var.regions
+    name                = "${var.resource_prefix}-${var.short_names[each.key]}-webapp"
+    location            = each.value
+    resource_group_name = azurerm_resource_group.rg.name
+    app_service_plan_id = azurerm_app_service_plan.asp[each.key].id
 
-#     # site_config {
-#         # always_on           = true
-#         # default_documents   = [
-#         #     "Default.htm",
-#         #     "Default.html",
-#         #     "hostingstart.html"
-#         # ]
-#     # }
+    # site_config {
+        # always_on           = true
+        # default_documents   = [
+        #     "Default.htm",
+        #     "Default.html",
+        #     "hostingstart.html"
+        # ]
+    # }
 
-#     app_settings = {
-#         "storageContainerName"          = "${var.resource_prefix}-${var.short_names[each.key]}"
-#         "connectionString "             = "${azurerm_storage_account.storage[each.key].primary_connection_string}"
-#     }
-#     depends_on = [azurerm_storage_account.storage, azurerm_app_service_plan.asp]
+    app_settings = {
+        "storageContainerName"          = "${var.resource_prefix}-${var.short_names[each.key]}"
+        "connectionString "             = "${azurerm_storage_account.storage[each.key].primary_connection_string}"
+    }
+    depends_on = [azurerm_storage_account.storage, azurerm_app_service_plan.asp]
     
-# }
+}
 
 resource "azurerm_storage_account" "storage" {
     for_each = var.regions
