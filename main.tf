78a415d884ad5af9f79435bd2cb2742f355dd253
Matt Boyd <mjb141@gmail.com>
2020-12-09 11:06:08 +0000

    Adding FrontDoor back in

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_app_service_plan" "asp" {
     }
 }
 
-# Provision the Azure App Service to host the main web site
+# Provision the Azure App Service to host the Website
 resource "azurerm_app_service" "webapp" {
     for_each = var.regions
     name                = "${var.resource_prefix}-${var.short_names[each.key]}-webapp"
@@ -59,6 +59,7 @@ resource "azurerm_app_service" "webapp" {
     
 }
 
+# Provision the Azure Storage Account 
 resource "azurerm_storage_account" "storage" {
     for_each = var.regions
     name                     = replace(lower("${var.resource_prefix}-${var.short_names[each.key]}-sa"), "-", "")
@@ -68,53 +69,54 @@ resource "azurerm_storage_account" "storage" {
     account_replication_type = "GRS"
 }
 
-# resource "azurerm_frontdoor" "frontdoor" {
-#   name                                         = "${var.resource_prefix}-frontdoor"
-#   #location                                     = "Global"
-#   resource_group_name                          = azurerm_resource_group.rg.name
-#   enforce_backend_pools_certificate_name_check = false
+# Provision the Azure FrontDoor 
+resource "azurerm_frontdoor" "frontdoor" {
+  name                                         = "${var.resource_prefix}-frontdoor"
+  #location                                     = "Global"
+  resource_group_name                          = azurerm_resource_group.rg.name
+  enforce_backend_pools_certificate_name_check = false
 
-#   routing_rule {
-#     name               = "${var.resource_prefix}-RoutingRule1"
-#     accepted_protocols = ["Http", "Https"]
-#     patterns_to_match  = ["/*"]
-#     frontend_endpoints = ["${var.resource_prefix}-FrontendEndpoint1"]
-#     forwarding_configuration {
-#       forwarding_protocol = "MatchRequest"
-#       backend_pool_name   = "${var.resource_prefix}-Backend"
-#     }
-#   }
+  routing_rule {
+    name               = "${var.resource_prefix}-RoutingRule1"
+    accepted_protocols = ["Http", "Https"]
+    patterns_to_match  = ["/*"]
+    frontend_endpoints = ["${var.resource_prefix}-FrontendEndpoint1"]
+    forwarding_configuration {
+      forwarding_protocol = "MatchRequest"
+      backend_pool_name   = "${var.resource_prefix}-Backend"
+    }
+  }
 
-#   backend_pool_load_balancing {
-#     name = "${var.resource_prefix}-LoadBalancingSettings1"
-#   }
+  backend_pool_load_balancing {
+    name = "${var.resource_prefix}-LoadBalancingSettings1"
+  }
 
-#   backend_pool_health_probe {
-#     name = "${var.resource_prefix}-HealthProbeSetting1"
-#   }
+  backend_pool_health_probe {
+    name = "${var.resource_prefix}-HealthProbeSetting1"
+  }
 
-#   backend_pool {
-#     name = "${var.resource_prefix}-Backend"
+  backend_pool {
+    name = "${var.resource_prefix}-Backend"
     
-#     dynamic backend {
-#       for_each = var.regions
+    dynamic backend {
+      for_each = var.regions
 
-#       content {
-#         host_header = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
-#         address     = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
-#         http_port   = 80
-#         https_port  = 443
-#       }
-#     }
+      content {
+        host_header = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
+        address     = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
+        http_port   = 80
+        https_port  = 443
+      }
+    }
 
-#     load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
-#     health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
-#   }
+    load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
+    health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
+  }
 
-#   frontend_endpoint {
-#     name                              = "${var.resource_prefix}-FrontendEndpoint1"
-#     host_name                         = "${var.resource_prefix}-frontdoor.azurefd.net"
-#     custom_https_provisioning_enabled = false
-#   }
-#   depends_on = [azurerm_app_service.webapp]
-# }
+  frontend_endpoint {
+    name                              = "${var.resource_prefix}-FrontendEndpoint1"
+    host_name                         = "${var.resource_prefix}-frontdoor.azurefd.net"
+    custom_https_provisioning_enabled = false
+  }
+  depends_on = [azurerm_app_service.webapp]
+}
