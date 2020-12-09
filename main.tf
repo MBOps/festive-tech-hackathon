0434b1f299ff1991a6e25d21da05c1fdc0e58d42
Matt Boyd <mjb141@gmail.com>
2020-12-09 16:06:00 +0000

    Removing FrontDoor for now

--- main.tf
+++ main.tf
@@ -70,60 +70,60 @@ resource "azurerm_storage_account" "storage" {
 }
 
 # Provision the Azure FrontDoor
-resource "azurerm_frontdoor" "frontdoor" {
-  name                                         = "${var.resource_prefix}-frontdoor"
-  resource_group_name                          = azurerm_resource_group.rg.name
-  enforce_backend_pools_certificate_name_check = false
+# resource "azurerm_frontdoor" "frontdoor" {
+#   name                                         = "${var.resource_prefix}-frontdoor"
+#   resource_group_name                          = azurerm_resource_group.rg.name
+#   enforce_backend_pools_certificate_name_check = false
 
-  routing_rule {
-    name               = "${var.resource_prefix}-RoutingRule1"
-    accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = ["/*"]
-    frontend_endpoints = ["${var.resource_prefix}-FrontendEndpoint1"]
-    forwarding_configuration {
-      forwarding_protocol = "MatchRequest"
-      backend_pool_name   = "${var.resource_prefix}-Backend"
-    }
-  }
+#   routing_rule {
+#     name               = "${var.resource_prefix}-RoutingRule1"
+#     accepted_protocols = ["Http", "Https"]
+#     patterns_to_match  = ["/*"]
+#     frontend_endpoints = ["${var.resource_prefix}-FrontendEndpoint1"]
+#     forwarding_configuration {
+#       forwarding_protocol = "MatchRequest"
+#       backend_pool_name   = "${var.resource_prefix}-Backend"
+#     }
+#   }
 
-  backend_pool_load_balancing {
-    name = "${var.resource_prefix}-LoadBalancingSettings1"
-  }
+#   backend_pool_load_balancing {
+#     name = "${var.resource_prefix}-LoadBalancingSettings1"
+#   }
 
-  backend_pool_health_probe {
-    name = "${var.resource_prefix}-HealthProbeSetting1"
-  }
+#   backend_pool_health_probe {
+#     name = "${var.resource_prefix}-HealthProbeSetting1"
+#   }
 
-  backend_pool {
-    name = "${var.resource_prefix}-Backend"
+#   backend_pool {
+#     name = "${var.resource_prefix}-Backend"
     
-    dynamic backend {
-      for_each = var.regionstest
+#     dynamic backend {
+#       for_each = var.regionstest
 
-      content {
-        host_header = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
-        address     = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
-        http_port   = 80
-        https_port  = 443
-      }
-    }
+#       content {
+#         host_header = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
+#         address     = "${var.resource_prefix}-${var.short_names[backend.key]}-webapp.azurewebsites.net"
+#         http_port   = 80
+#         https_port  = 443
+#       }
+#     }
 
-    load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
-    health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
-  }
+#     load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
+#     health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
+#   }
 
-  frontend_endpoint {
-    name                              = "${var.resource_prefix}-FrontendEndpoint1"
-    host_name                         = "${var.resource_prefix}-frontdoor.azurefd.net"
-    custom_https_provisioning_enabled = false
-  }
 #   frontend_endpoint {
-#     name                              = "${var.resource_prefix}-FrontendEndpoint2"
-#     host_name                         = "${var.resource_prefix}.com"
+#     name                              = "${var.resource_prefix}-FrontendEndpoint1"
+#     host_name                         = "${var.resource_prefix}-frontdoor.azurefd.net"
 #     custom_https_provisioning_enabled = false
 #   }
-  depends_on = [azurerm_app_service.webapp]
-}
+# #   frontend_endpoint {
+# #     name                              = "${var.resource_prefix}-FrontendEndpoint2"
+# #     host_name                         = "${var.resource_prefix}.com"
+# #     custom_https_provisioning_enabled = false
+# #   }
+#   depends_on = [azurerm_app_service.webapp]
+# }
 
 resource "azurerm_app_service" "dockerapp" {
   for_each = var.regionstest
