97c9a63ee0200ccaf5139037aa1474b86c12a496
Matt Boyd <mjb141@gmail.com>
2020-12-10 19:30:01 +0000

    updating TLS version

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
-  min_tls_version          = "1.2"
+  min_tls_version          = "TLS1_2"
   allow_blob_public_access = false
 }
 
