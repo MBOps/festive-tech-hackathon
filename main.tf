edb888c1749034628549f08ef7a44bd764aed8dd
Matt Boyd <mjb141@gmail.com>
2020-12-10 19:53:26 +0000

    updating formatting

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "azurerm_subnet" "internal" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.1.1.0/24"]
-  service_endpoints = ["Microsoft.Storage"]
+  service_endpoints    = ["Microsoft.Storage"]
 }
 
 # Provision the Azure Storage Account 
