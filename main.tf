a4ec6ed86e09710553e2051ea9043e3191ed72da
Matt Boyd <mjb141@gmail.com>
2020-12-10 19:26:55 +0000

    modify storage account

--- main.tf
+++ main.tf
@@ -56,6 +56,8 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  min_tls_version          = "1.2"
+  allow_blob_public_access = false
 }
 
 # Provision the Azure FrontDoor
