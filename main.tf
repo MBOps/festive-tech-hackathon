3e48220b637cdea785638eae8897cb6262577e75
Matt Boyd <mjb141@gmail.com>
2020-12-11 20:30:44 +0000

    trying non-docker deploy

--- main.tf
+++ main.tf
@@ -134,17 +134,17 @@ resource "azurerm_app_service" "webapp" {
     "connectionString"     = "${azurerm_storage_account.storage[each.key].primary_connection_string}"
 
     # Settings for private Container Registires  
-    DOCKER_REGISTRY_SERVER_URL      = "https://${var.registry_name}"
-    DOCKER_REGISTRY_SERVER_USERNAME = "${var.admin_username}"
-    DOCKER_REGISTRY_SERVER_PASSWORD = "${var.admin_password}"
-    # APPINSIGHTS_INSTRUMENTATIONKEY  = "${azurerm_application_insights.appinsights.instrumentation_key}"
+    # DOCKER_REGISTRY_SERVER_URL      = "https://${var.registry_name}"
+    # DOCKER_REGISTRY_SERVER_USERNAME = "${var.admin_username}"
+    # DOCKER_REGISTRY_SERVER_PASSWORD = "${var.admin_password}"
+    APPINSIGHTS_INSTRUMENTATIONKEY  = "${azurerm_application_insights.appinsights.instrumentation_key}"
   }
 
   # Configure Docker Image to load on start
-  #   site_config {
-  #     linux_fx_version = "DOCKER|${var.registry_name}/festive-tech:${var.tag_name}"
-  #     always_on        = "true"
-  #   }
+    site_config {
+    #   linux_fx_version = "DOCKER|${var.registry_name}/festive-tech:${var.tag_name}"
+      always_on        = "true"
+    }
 
   identity {
     type = "SystemAssigned"
@@ -160,12 +160,12 @@ resource "azurerm_app_service_virtual_network_swift_connection" "vnetconnection"
   depends_on     = [azurerm_subnet.internal, azurerm_app_service.webapp]
 }
 
-# resource "azurerm_application_insights" "appinsights" {
-#   name                = "${var.resource_prefix}-appinsights"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   application_type    = "web"
-# }
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.resource_prefix}-appinsights"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
 
 resource "azurerm_monitor_autoscale_setting" "autoscaling" {
   for_each            = var.regions
