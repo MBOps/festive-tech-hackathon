500a1235d44b286043a9faa502571544d5a04688
Matt Boyd <mjb141@gmail.com>
2020-12-11 13:06:02 +0000

    Adding Rules back in with CPUPercentage

--- main.tf
+++ main.tf
@@ -183,52 +183,52 @@ resource "azurerm_monitor_autoscale_setting" "autoscaling" {
       maximum = 10
     }
 
-    # rule {
-    #   metric_trigger {
-    #     metric_name        = "Percentage CPU"
-    #     metric_resource_id = azurerm_app_service_plan.asp[each.key].id
-    #     time_grain         = "PT1M"
-    #     statistic          = "Average"
-    #     time_window        = "PT5M"
-    #     time_aggregation   = "Average"
-    #     operator           = "GreaterThan"
-    #     threshold          = 75
-    #   }
+    rule {
+      metric_trigger {
+        metric_name        = "CPUPercentage"
+        metric_resource_id = azurerm_app_service_plan.asp[each.key].id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+      }
 
-    #   scale_action {
-    #     direction = "Increase"
-    #     type      = "ChangeCount"
-    #     value     = "1"
-    #     cooldown  = "PT1M"
-    #   }
-    # }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
 
-    # rule {
-    #   metric_trigger {
-    #     metric_name        = "Percentage CPU"
-    #     metric_resource_id = azurerm_app_service_plan.asp[each.key].id
-    #     time_grain         = "PT1M"
-    #     statistic          = "Average"
-    #     time_window        = "PT5M"
-    #     time_aggregation   = "Average"
-    #     operator           = "LessThan"
-    #     threshold          = 25
-    #   }
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_app_service_plan.asp[each.key].id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
 
-    #   scale_action {
-    #     direction = "Decrease"
-    #     type      = "ChangeCount"
-    #     value     = "1"
-    #     cooldown  = "PT1M"
-    #   }
-    # }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
   }
 
-  #   notification {
-  #     email {
-  #       send_to_subscription_administrator    = true
-  #       send_to_subscription_co_administrator = true
-  #       custom_emails                         = [""]
-  #     }
-  #   }
+    notification {
+      email {
+        send_to_subscription_administrator    = true
+        send_to_subscription_co_administrator = true
+        custom_emails                         = [""]
+      }
+    }
 }
