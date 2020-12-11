71047d69e94d19ce81b3e1b589220b78e31fe1be
Matt Boyd <mjb141@gmail.com>
2020-12-11 11:34:18 +0000

    fixing format again

--- main.tf
+++ main.tf
@@ -174,61 +174,61 @@ resource "azurerm_monitor_autoscale_setting" "autoscaling" {
   resource_group_name = azurerm_resource_group.rg.name
   target_resource_id  = azurerm_app_service.webapp[each.key].id
 
-    profile {
-      name = "defaultProfile"
+  profile {
+    name = "defaultProfile"
 
-      capacity {
-        default = 1
-        minimum = 1
-        maximum = 10
-      }
-
-      rule {
-        metric_trigger {
-          metric_name        = "Percentage CPU"
-          metric_resource_id = azurerm_app_service.webapp[each.key].id
-          time_grain         = "PT1M"
-          statistic          = "Average"
-          time_window        = "PT5M"
-          time_aggregation   = "Average"
-          operator           = "GreaterThan"
-          threshold          = 75
-        }
-
-        scale_action {
-          direction = "Increase"
-          type      = "ChangeCount"
-          value     = "1"
-          cooldown  = "PT1M"
-        }
-      }
-
-    #   rule {
-    #     metric_trigger {
-    #       metric_name        = "Percentage CPU"
-    #       metric_resource_id = azurerm_virtual_machine_scale_set.example.id
-    #       time_grain         = "PT1M"
-    #       statistic          = "Average"
-    #       time_window        = "PT5M"
-    #       time_aggregation   = "Average"
-    #       operator           = "LessThan"
-    #       threshold          = 25
-    #     }
-
-    #     scale_action {
-    #       direction = "Decrease"
-    #       type      = "ChangeCount"
-    #       value     = "1"
-    #       cooldown  = "PT1M"
-    #     }
-    #   }
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 10
     }
 
-  #   notification {
-  #     email {
-  #       send_to_subscription_administrator    = true
-  #       send_to_subscription_co_administrator = true
-  #       custom_emails                         = ["admin@contoso.com"]
-  #     }
-  #   }
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_app_service.webapp[each.key].id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_app_service.webapp[each.key].id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+
+  notification {
+    email {
+      send_to_subscription_administrator    = true
+      send_to_subscription_co_administrator = true
+      custom_emails                         = [""]
+    }
+  }
 }
