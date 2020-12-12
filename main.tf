dd4f364ef9c12d0a2d4a985321d14db6f6c63612
Matt Boyd <mjb141@gmail.com>
2020-12-12 20:36:30 +0000

    test

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "rg" {
 # Provision the App Service plan to host the App Service web app in each region
 resource "azurerm_app_service_plan" "asp" {
   for_each            = var.regions
-  name                = "${var.resource_prefix}-${tostring(element([each.value], 1))}-asp"
+  name                = "${var.resource_prefix}-${each.value[1]}-asp"
   location            = each.value
   resource_group_name = azurerm_resource_group.rg.name
   #   kind                = "Windows"
@@ -54,127 +54,127 @@ resource "azurerm_app_service_plan" "asp" {
 # }
 
 # Provision the Azure Storage Account 
-resource "azurerm_storage_account" "storage" {
-  for_each                 = var.regions
-  name                     = replace(lower("${var.resource_prefix}-${tostring(element([each.value], 1))}-sa"), "-", "")
-  location                 = each.value
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  min_tls_version          = "TLS1_2"
-  #   allow_blob_public_access = false
+# resource "azurerm_storage_account" "storage" {
+#   for_each                 = var.regions
+#   name                     = replace(lower("${var.resource_prefix}-${tostring(element([each.value], 1))}-sa"), "-", "")
+#   location                 = each.value
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+#   min_tls_version          = "TLS1_2"
+#   #   allow_blob_public_access = false
 
-  #   network_rules {
-  #     default_action             = "Deny"
-  #     virtual_network_subnet_ids = [azurerm_subnet.internal[each.key].id]
-  #   }
-  #depends_on = [azurerm_subnet.internal]
-}
+#   #   network_rules {
+#   #     default_action             = "Deny"
+#   #     virtual_network_subnet_ids = [azurerm_subnet.internal[each.key].id]
+#   #   }
+#   #depends_on = [azurerm_subnet.internal]
+# }
 
-# Provision the Azure FrontDoor
-resource "azurerm_frontdoor" "frontdoor" {
-  name                                         = "${var.resource_prefix}-frontdoor"
-  resource_group_name                          = azurerm_resource_group.rg.name
-  enforce_backend_pools_certificate_name_check = false
+# # Provision the Azure FrontDoor
+# resource "azurerm_frontdoor" "frontdoor" {
+#   name                                         = "${var.resource_prefix}-frontdoor"
+#   resource_group_name                          = azurerm_resource_group.rg.name
+#   enforce_backend_pools_certificate_name_check = false
 
-  routing_rule {
-    name               = "${var.resource_prefix}-RoutingRule1"
-    accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = ["/*"]
-    frontend_endpoints = ["${var.resource_prefix}-FrontendEndpoint1"]
-    forwarding_configuration {
-      forwarding_protocol = "MatchRequest"
-      backend_pool_name   = "${var.resource_prefix}-Backend"
-    }
-  }
+#   routing_rule {
+#     name               = "${var.resource_prefix}-RoutingRule1"
+#     accepted_protocols = ["Http", "Https"]
+#     patterns_to_match  = ["/*"]
+#     frontend_endpoints = ["${var.resource_prefix}-FrontendEndpoint1"]
+#     forwarding_configuration {
+#       forwarding_protocol = "MatchRequest"
+#       backend_pool_name   = "${var.resource_prefix}-Backend"
+#     }
+#   }
 
-  backend_pool_load_balancing {
-    name = "${var.resource_prefix}-LoadBalancingSettings1"
-  }
+#   backend_pool_load_balancing {
+#     name = "${var.resource_prefix}-LoadBalancingSettings1"
+#   }
 
-  backend_pool_health_probe {
-    name = "${var.resource_prefix}-HealthProbeSetting1"
-  }
+#   backend_pool_health_probe {
+#     name = "${var.resource_prefix}-HealthProbeSetting1"
+#   }
 
-  backend_pool {
-    name = "${var.resource_prefix}-Backend"
+#   backend_pool {
+#     name = "${var.resource_prefix}-Backend"
 
-    dynamic "backend" {
-      for_each = var.regions
+#     dynamic "backend" {
+#       for_each = var.regions
 
-      content {
-        host_header = "${azurerm_app_service.webapp[backend.key].name}.azurewebsites.net"
-        address     = "${azurerm_app_service.webapp[backend.key].name}.azurewebsites.net"
-        http_port   = 80
-        https_port  = 443
-      }
-    }
+#       content {
+#         host_header = "${azurerm_app_service.webapp[backend.key].name}.azurewebsites.net"
+#         address     = "${azurerm_app_service.webapp[backend.key].name}.azurewebsites.net"
+#         http_port   = 80
+#         https_port  = 443
+#       }
+#     }
 
-    load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
-    health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
-  }
+#     load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
+#     health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
+#   }
 
-  #   dynamic "backend_pool" {
-  #     for_each = var.regions
+#   #   dynamic "backend_pool" {
+#   #     for_each = var.regions
 
-  #     content {
-  #       name                = "${var.resource_prefix}-Backend"
-  #       load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
-  #       health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
+#   #     content {
+#   #       name                = "${var.resource_prefix}-Backend"
+#   #       load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
+#   #       health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
 
-  #       backend {
-  #         host_header = "${azurerm_app_service.webapp[backend_pool.key].name}.azurewebsites.net"
-  #         address     = "${azurerm_app_service.webapp[backend_pool.key].name}.azurewebsites.net"
-  #         http_port   = 80
-  #         https_port  = 443
-  #       }
-  #     }
+#   #       backend {
+#   #         host_header = "${azurerm_app_service.webapp[backend_pool.key].name}.azurewebsites.net"
+#   #         address     = "${azurerm_app_service.webapp[backend_pool.key].name}.azurewebsites.net"
+#   #         http_port   = 80
+#   #         https_port  = 443
+#   #       }
+#   #     }
 
-  #   }
+#   #   }
 
-  frontend_endpoint {
-    name                              = "${var.resource_prefix}-FrontendEndpoint1"
-    host_name                         = "${var.resource_prefix}-frontdoor.azurefd.net"
-    custom_https_provisioning_enabled = false
-  }
-  depends_on = [azurerm_app_service.webapp]
-}
+#   frontend_endpoint {
+#     name                              = "${var.resource_prefix}-FrontendEndpoint1"
+#     host_name                         = "${var.resource_prefix}-frontdoor.azurefd.net"
+#     custom_https_provisioning_enabled = false
+#   }
+#   depends_on = [azurerm_app_service.webapp]
+# }
 
 # locals {
 #   distinctregions = distinct(split("-", var.regions.key)[0])
 # }
 
-resource "azurerm_app_service" "webapp" {
-  for_each            = var.regions
-  name                = "${var.resource_prefix}-${tostring(element([each.value], 1))}-webapp"
-  location            = each.value
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp[each.key].id
+# resource "azurerm_app_service" "webapp" {
+#   for_each            = var.regions
+#   name                = "${var.resource_prefix}-${tostring(element([each.value], 1))}-webapp"
+#   location            = each.value
+#   resource_group_name = azurerm_resource_group.rg.name
+#   app_service_plan_id = azurerm_app_service_plan.asp[each.key].id
 
-  # Do not attach Storage by default
-  app_settings = {
-    "storageContainerName" = "${var.resource_prefix}-${tostring(element([each.value], 1))}"
-    "connectionString"     = "${azurerm_storage_account.storage[each.key].primary_connection_string}"
+#   # Do not attach Storage by default
+#   app_settings = {
+#     "storageContainerName" = "${var.resource_prefix}-${tostring(element([each.value], 1))}"
+#     "connectionString"     = "${azurerm_storage_account.storage[each.key].primary_connection_string}"
 
-    # Settings for private Container Registires  
-    DOCKER_REGISTRY_SERVER_URL      = "https://${var.registry_name}"
-    DOCKER_REGISTRY_SERVER_USERNAME = "${var.admin_username}"
-    DOCKER_REGISTRY_SERVER_PASSWORD = "${var.admin_password}"
-    # APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.appinsights.instrumentation_key}"
-  }
+#     # Settings for private Container Registires  
+#     DOCKER_REGISTRY_SERVER_URL      = "https://${var.registry_name}"
+#     DOCKER_REGISTRY_SERVER_USERNAME = "${var.admin_username}"
+#     DOCKER_REGISTRY_SERVER_PASSWORD = "${var.admin_password}"
+#     # APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.appinsights.instrumentation_key}"
+#   }
 
-  # Configure Docker Image to load on start
-  site_config {
-    linux_fx_version = "DOCKER|${var.registry_name}/festive-tech:${var.tag_name}"
-    always_on        = "true"
-  }
+#   # Configure Docker Image to load on start
+#   site_config {
+#     linux_fx_version = "DOCKER|${var.registry_name}/festive-tech:${var.tag_name}"
+#     always_on        = "true"
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  depends_on = [azurerm_storage_account.storage, azurerm_app_service_plan.asp]
-}
+#   depends_on = [azurerm_storage_account.storage, azurerm_app_service_plan.asp]
+# }
 
 # resource "azurerm_app_service_virtual_network_swift_connection" "vnetconnection" {
 #   for_each       = var.regions
@@ -190,67 +190,67 @@ resource "azurerm_app_service" "webapp" {
 #   application_type    = "web"
 # }
 
-resource "azurerm_monitor_autoscale_setting" "autoscaling" {
-  for_each            = var.regions
-  name                = "${var.resource_prefix}-${tostring(element([each.value], 1))}-scaling"
-  location            = each.value
-  resource_group_name = azurerm_resource_group.rg.name
-  target_resource_id  = azurerm_app_service_plan.asp[each.key].id
+# resource "azurerm_monitor_autoscale_setting" "autoscaling" {
+#   for_each            = var.regions
+#   name                = "${var.resource_prefix}-${tostring(element([each.value], 1))}-scaling"
+#   location            = each.value
+#   resource_group_name = azurerm_resource_group.rg.name
+#   target_resource_id  = azurerm_app_service_plan.asp[each.key].id
 
-  profile {
-    name = "defaultProfile"
+#   profile {
+#     name = "defaultProfile"
 
-    capacity {
-      default = 1
-      minimum = 1
-      maximum = 10
-    }
+#     capacity {
+#       default = 1
+#       minimum = 1
+#       maximum = 10
+#     }
 
-    rule {
-      metric_trigger {
-        metric_name        = "CPUPercentage"
-        metric_resource_id = azurerm_app_service_plan.asp[each.key].id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "GreaterThan"
-        threshold          = 75
-      }
+#     rule {
+#       metric_trigger {
+#         metric_name        = "CPUPercentage"
+#         metric_resource_id = azurerm_app_service_plan.asp[each.key].id
+#         time_grain         = "PT1M"
+#         statistic          = "Average"
+#         time_window        = "PT5M"
+#         time_aggregation   = "Average"
+#         operator           = "GreaterThan"
+#         threshold          = 75
+#       }
 
-      scale_action {
-        direction = "Increase"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
+#       scale_action {
+#         direction = "Increase"
+#         type      = "ChangeCount"
+#         value     = "1"
+#         cooldown  = "PT1M"
+#       }
+#     }
 
-    rule {
-      metric_trigger {
-        metric_name        = "CPUPercentage"
-        metric_resource_id = azurerm_app_service_plan.asp[each.key].id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "LessThan"
-        threshold          = 25
-      }
+#     rule {
+#       metric_trigger {
+#         metric_name        = "CPUPercentage"
+#         metric_resource_id = azurerm_app_service_plan.asp[each.key].id
+#         time_grain         = "PT1M"
+#         statistic          = "Average"
+#         time_window        = "PT5M"
+#         time_aggregation   = "Average"
+#         operator           = "LessThan"
+#         threshold          = 25
+#       }
 
-      scale_action {
-        direction = "Decrease"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
-  }
+#       scale_action {
+#         direction = "Decrease"
+#         type      = "ChangeCount"
+#         value     = "1"
+#         cooldown  = "PT1M"
+#       }
+#     }
+#   }
 
-  notification {
-    email {
-      send_to_subscription_administrator    = true
-      send_to_subscription_co_administrator = true
-    }
-  }
-}
+#   notification {
+#     email {
+#       send_to_subscription_administrator    = true
+#       send_to_subscription_co_administrator = true
+#     }
+#   }
+# }
