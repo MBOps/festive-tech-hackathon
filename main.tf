569b55d2d25de5c8676a92a23db7b871693f3f63
Matt Boyd <mjb141@gmail.com>
2020-12-12 20:50:11 +0000

    test

--- main.tf
+++ main.tf
@@ -54,22 +54,22 @@ resource "azurerm_app_service_plan" "asp" {
 # }
 
 # Provision the Azure Storage Account 
-# resource "azurerm_storage_account" "storage" {
-#   for_each                 = var.regions
-#   name                     = replace(lower("${var.resource_prefix}-${tostring(element([each.value], 1))}-sa"), "-", "")
-#   location                 = each.value
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
-#   min_tls_version          = "TLS1_2"
-#   #   allow_blob_public_access = false
+resource "azurerm_storage_account" "storage" {
+  for_each                 = var.regions
+  name                     = replace(lower("${var.resource_prefix}-${each.value.shortname}-sa"), "-", "")
+  location                 = each.value.name
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  min_tls_version          = "TLS1_2"
+  #   allow_blob_public_access = false
 
-#   #   network_rules {
-#   #     default_action             = "Deny"
-#   #     virtual_network_subnet_ids = [azurerm_subnet.internal[each.key].id]
-#   #   }
-#   #depends_on = [azurerm_subnet.internal]
-# }
+  #   network_rules {
+  #     default_action             = "Deny"
+  #     virtual_network_subnet_ids = [azurerm_subnet.internal[each.key].id]
+  #   }
+  #depends_on = [azurerm_subnet.internal]
+}
 
 # # Provision the Azure FrontDoor
 # resource "azurerm_frontdoor" "frontdoor" {
