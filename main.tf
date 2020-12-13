8edd0fc6ff944dd9ca9f0e43f60120eaa3134cc6
Matt Boyd <mjb141@gmail.com>
2020-12-13 10:47:05 +0000

    test

--- main.tf
+++ main.tf
@@ -142,18 +142,14 @@ resource "azurerm_storage_account" "storage" {
 
 locals {
   #distinctregions = distinct([for region in var.regions : region])
-  allregions = flatten(var.geos)
+  #allregions = flatten(var.geos)
 
-  #   allregions = flatten([
-  #     for geo in var.geos : [
-  #       for region_key in geos.regions : {
-  #         region_key = region_key
-  #         name       = region.name,
-  #         shortname  = region.shortname,
-  #         region     = geo.geo
-  #       }
-  #     ]
-  #   ])
+  allregions = flatten([
+    for geo in var.geos : [
+      for region_key in geos.regions : {
+      }
+    ]
+  ])
 }
 
 output "instance_ip_addr" {
