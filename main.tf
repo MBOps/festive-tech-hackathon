2f7d6f514ca7ca50d3c9b78e59d2924b798b4b84
Matt Boyd <mjb141@gmail.com>
2020-12-13 22:32:00 +0000

    test

--- main.tf
+++ main.tf
@@ -119,7 +119,7 @@ resource "azurerm_frontdoor" "frontdoor" {
     #for_each = var.geos
     for_each = { for geo in var.geos2 : geo.geo_id => geo }
     content {
-      name                = "${var.resource_prefix}-${[backend_pool.key]}-Backend"
+      name                = "${var.resource_prefix}-${[backend_pool.value.geo_id]}-Backend"
       load_balancing_name = "${var.resource_prefix}-LoadBalancingSettings1"
       health_probe_name   = "${var.resource_prefix}-HealthProbeSetting1"
 
